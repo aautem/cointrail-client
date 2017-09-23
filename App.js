353335a0dea670680f42dc20fb3e86459a25470c
Alex Autem <aautem@trifinlabs.com>
2017-09-23 15:20:51 -0700

    Add game methods to app container

--- App.js
+++ App.js
@@ -1,21 +1,45 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Game from './src/components/game-container';
+import NavContainer from './src/components/nav-container';
+import MainMenuContainer from './src/components/main-menu-container';
+import GameContainer from './src/components/game-container';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
       user: 'aautem',
       gameInProgress: false,
       stats: null,
     };
+
+    this.startNewGame = this.startNewGame.bind(this);
+    this.quitGame = this.quitGame.bind(this);
+  }
+
+  startNewGame() {
+    this.setState((state) => {
+      state.gameInProgress = true;
+      return state;
+    });
+  }
+
+  quitGame() {
+    this.setState((state) => {
+      state.gameInProgress = false;
+      return state;
+    });
   }
 
   render() {
     return (
       <View style={styles.appContainer}>
-        <Game size={4} theme={'default'} series={3} />
+        <NavContainer quitGame={this.quitGame} />
+        {!this.state.gameInProgress &&
+        <MainMenuContainer startNewGame={this.startNewGame} />}
+        {this.state.gameInProgress &&
+        <GameContainer size={4} theme={'default'} series={3} />}
       </View>
     );
   }
