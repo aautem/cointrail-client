bb9ea0f9229d6ae4500ad76dd781ace139819457
Alex Autem <aautem@trifinlabs.com>
2017-09-23 01:02:08 -0700

    Add title placeholder

--- src/components/nav-container.js
+++ src/components/nav-container.js
@@ -5,6 +5,7 @@ export default class NavContainer extends React.Component {
   render() {
     return (
       <View style={styles.navContainer}>
+        <Text style={styles.titleText}>{'.~::  C O N T R A I L S ::~.'}</Text>
       </View>
     );
   }
@@ -14,5 +15,12 @@ const styles = StyleSheet.create({
   navContainer: {
     flex: 1,
     backgroundColor: 'grey',
+  },
+  titleText: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    lineHeight: 100,
+    fontSize: 20,
   },
 });
