bc36709a58076522a8bfe155911f31d36388d972
Alex Autem <aautem@trifinlabs.com>
2017-09-23 05:47:25 -0700

    Add icons to coins

--- src/components/coin.js
+++ src/components/coin.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Icon } from 'react-native-elements';
 
 // key: React string
 // player: number
+// points: number
 
 export default class Coin extends React.Component {
   getColor() {
@@ -12,11 +14,32 @@ export default class Coin extends React.Component {
     return `player${this.props.player}`;
   }
 
+  getPointsIcon() {
+    if (this.props.points === 10) {
+      return (<Icon iconStyle={styles.icon} type='entypo' name='bug' color='steelblue' size={18} />);
+    } else if (this.props.points === 25) {
+      return (<Icon iconStyle={styles.icon} type='entypo' name='flower' color='steelblue' size={18} />);
+    } else if (this.props.points === 50) {
+      return (<Icon iconStyle={styles.icon} type='entypo' name='star-outlined' color='steelblue' size={18} />);
+    } else if (this.props.points === 100) {
+      return (<Icon iconStyle={styles.icon} type='entypo' name='baidu' color='steelblue' size={18} />);
+    } else if (this.props.points === 150) {
+      return (<Icon iconStyle={styles.icon} type='entypo' name='ticket' color='steelblue' size={18} />);
+    }
+    return null;
+  }
+
+  // {this.props.player === 0 &&
+  //   <Text style={styles.points}>{this.props.points}</Text>
+  // }
+
   render() {
     const color = this.getColor();
-
+    const icon = this.getPointsIcon();
     return (
-      <View style={[styles.coin, styles[color]]} />
+      <View style={[styles.coin, styles[color]]}>
+        {this.props.player === 0 && this.getPointsIcon()}
+      </View>
     );
   }
 }
@@ -25,6 +48,8 @@ const styles = StyleSheet.create({
   coin: {
     flex: 1,
     borderRadius: 100,
+    borderColor: 'black',
+    borderWidth: 1,
     margin: '3%',
   },
   player1: {
@@ -36,4 +61,15 @@ const styles = StyleSheet.create({
   empty: {
     backgroundColor: 'white',
   },
+  icon: {
+    paddingTop: 20,
+    backgroundColor: 'transparent',
+  },
+  points: {
+    color: 'grey',
+    fontSize: 10,
+    textAlign: 'center',
+    paddingTop: 25,
+    backgroundColor: 'transparent',
+  }
 });
