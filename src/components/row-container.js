ebe492b9405f79c88be0e93e1e0468033007aac3
Alex Autem <aautem@trifinlabs.com>
2017-09-23 05:50:28 -0700

    Add point values for coins

--- src/components/row-container.js
+++ src/components/row-container.js
@@ -4,6 +4,7 @@ import Coin from './coin';
 
 // key: React string
 // row: Gameboard Row
+// rowPoints: number[]
 
 export default class RowContainer extends React.Component {
   render() {
@@ -11,7 +12,7 @@ export default class RowContainer extends React.Component {
       <View style={styles.rowContainer}>
         {this.props.row.map((player, index) => {
           return (
-            <Coin key={`col-${index}`} player={player} />
+            <Coin key={`col-${index}`} player={player} points={this.props.rowPoints[index]} />
           );
         })}
       </View>
