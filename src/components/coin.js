3915fd4da2b9c86436ebe18907f2a682bf94ce4e
Alex Autem <aautem@trifinlabs.com>
2017-09-23 01:00:28 -0700

    Update coin style

--- src/components/coin.js
+++ src/components/coin.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+// key: React string
+// player: number
+
 export default class Coin extends React.Component {
   getColor() {
     if (this.props.player === 0) {
@@ -22,7 +25,7 @@ const styles = StyleSheet.create({
   coin: {
     flex: 1,
     borderRadius: 100,
-    margin: '5%',
+    margin: '3%',
   },
   player1: {
     backgroundColor: 'blue',
