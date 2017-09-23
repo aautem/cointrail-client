0c1671343a3b5b25c4f1c04b75d4b67240f275a7
Alex Autem <aautem@trifinlabs.com>
2017-09-23 01:01:41 -0700

    Make number of drop buttons dynamic

--- src/components/drop-zone-container.js
+++ src/components/drop-zone-container.js
@@ -2,13 +2,18 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import DropButton from './drop-button';
 
+// board: Gameboard
+// dropCoin: method
+
 export default class DropZoneContainer extends React.Component {
   render() {
     return (
       <View style={styles.rowContainer}>
-        <DropButton column={0} dropCoin={this.props.dropCoin} />
-        <DropButton column={1} dropCoin={this.props.dropCoin} />
-        <DropButton column={2} dropCoin={this.props.dropCoin} />
+        {this.props.board.map((row, index) => {
+          return (
+            <DropButton key={`drop-btn-${index}`} column={index} dropCoin={this.props.dropCoin} />
+          );
+        })}
       </View>
     );
   }
