c32712ce4b6c3fe05f359b573298a92ec137463f
Alex Autem <aautem@trifinlabs.com>
2017-09-24 20:46:21 -0700

    Add auth0 support

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import NavContainer from './src/components/nav-container';
 import MainMenuContainer from './src/components/main-menu-container';
 import GameContainer from './src/components/game-container';
+// import Auth0 from 'react-native-auth0';
+
+// GAME OPTIONS SUPPORT:
+// [X] Board size (4x4, 5x5, 6x6)
+// [ ] Series length (Best of 3 [w2], Best of 7 [w4], Best of 11 [w6])
+// [ ] Time Limit (15, 30, Off)
 
 export default class App extends React.Component {
   constructor(props) {
@@ -16,6 +22,20 @@ export default class App extends React.Component {
 
     this.startNewGame = this.startNewGame.bind(this);
     this.quitGame = this.quitGame.bind(this);
+  }
+
+  componentWillMount() {
+    // const auth0 = new Auth0({ domain: 'aautem.auth0.com', clientId: 'CXuCytj2Wgf5Om6M2sEfB80001Vd5N9j' });
+    // auth0
+    //   .webAuth
+    //   .authorize({scope: 'openid email', audience: 'https://aautem.auth0.com/userinfo'})
+    //   .then((credentials) => {
+    //     console.log(credentials);
+    //     // Successfully authenticated
+    //     // Store the accessToken
+    //   }).catch((error) => {
+    //     console.log(error);
+    //   });
   }
 
   startNewGame() {
