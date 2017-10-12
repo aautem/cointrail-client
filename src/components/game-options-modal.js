c904f97e4c95b8cca06060f8e3b2df7602a4a03c
Alex Autem <aautem@trifinlabs.com>
2017-10-11 20:16:10 -0700

    Update game option modal styles

--- src/components/game-options-modal.js
+++ src/components/game-options-modal.js
@@ -26,46 +26,85 @@ export default class GameOptionsModal extends React.Component {
         visible={this.props.visible}
         onRequestClose={this.props.closeGameOptionsModal}
       >
-        <View>
-          <Text>Select Game Options</Text>
+        <View style={{
+          flex: 1,
+          flexDirection: 'row',
+        }}>
+          <View style={{flex: 1}} />
 
-          <Text>BOARD SIZE: {this.state.boardSize}</Text>
-          <Picker
-            selectedValue={this.state.boardSize}
-            onValueChange={(size, index) => this.setState({boardSize: size})}>
-            <Picker.Item label='4 x 4' value={4} />
-            <Picker.Item label='5 x 5' value={5} />
-            <Picker.Item label='6 x 6' value={6} />
-          </Picker>
+          <View style={{flex: 4, flexDirection: 'column'}}>
+            <View style={{flex: 1}}>
+              <View style={{flex: 1}} />
 
-          <Text>SERIES LENGTH: {this.state.gameSeries}</Text>
-          <Slider
-            maximumValue={11}
-            minimumValue={3}
-            step={4}
-            value={7}
-            onSlidingComplete={(series) => this.setState({gameSeries: series})}
-          />
+              <View style={{flex: 1, alignContent: 'center'}}>
+                <Text>Select Game Options</Text>
+              </View>
 
-          <Text>TIME LIMIT: {this.state.timeLimit ? 'On' : 'Off'}</Text>
-          <Switch
-            value={this.state.timeLimit}
-            onValueChange={(timeLimit) => this.setState({timeLimit: timeLimit})}
-          />
+              <View style={{flex: 1, flexDirection: 'row', alignContent: 'center'}}>
+                <View style={{flex: 1, alignContent: 'center'}}>
+                  <Text>BOARD SIZE:</Text>
+                </View>
+                <View style={{flex: 1, alignContent: 'center'}}>
+                  <Picker
+                    selectedValue={this.state.boardSize}
+                    onValueChange={(size, index) => this.setState({boardSize: size})}>
+                    <Picker.Item label='4 x 4' value={4} />
+                    <Picker.Item label='5 x 5' value={5} />
+                    <Picker.Item label='6 x 6' value={6} />
+                  </Picker>
+                </View>
+              </View>
 
-          <Button
-            onPress={this.startGameWithSettings}
-            title='Play'
-            color="#841584"
-            accessibilityLabel="Play"
-          />
+              <View style={{flex: 1, flexDirection: 'row', alignContent: 'center'}}>
+                <View style={{flex: 1, alignContent: 'center'}}>
+                  <Text>{this.state.gameSeries} GAME SERIES</Text>
+                  <Slider
+                    maximumValue={11}
+                    minimumValue={3}
+                    step={4}
+                    value={7}
+                    onSlidingComplete={(series) => this.setState({gameSeries: series})}
+                  />
+                </View>
+              </View>
 
-          <Button
-            onPress={this.props.closeGameOptionsModal}
-            title='Cancel'
-            color="#841584"
-            accessibilityLabel="Cancel"
-          />
+              <View style={{flex: 1, flexDirection: 'row', alignContent: 'center'}}>
+                <View style={{flex: 1, alignContent: 'center'}}>
+                  <Text>TIME LIMIT:</Text>
+                </View>
+                <View style={{flex: 1, alignContent: 'center'}}>
+                  <Switch
+                    value={this.state.timeLimit}
+                    onValueChange={(timeLimit) => this.setState({timeLimit: timeLimit})}
+                  />
+                </View>
+              </View>
+
+              <View style={{flex: 1, flexDirection: 'row'}}>
+                <View style={{flex: 1}}>
+                  <Button
+                    onPress={this.startGameWithSettings}
+                    title='Play'
+                    color="#841584"
+                    accessibilityLabel="Play"
+                  />
+                </View>
+
+                <View style={{flex: 1}}>
+                  <Button
+                    onPress={this.props.closeGameOptionsModal}
+                    title='Cancel'
+                    color="#841584"
+                    accessibilityLabel="Cancel"
+                  />
+                </View>
+              </View>
+
+              <View style={{flex: 1}} />
+            </View>
+          </View>
+
+          <View style={{flex: 1}} />
         </View>
       </Modal>
     );
