47df39eb662693523761018b6a09ffc7820e2160
Alex Autem <autem.alex@gmail.com>
2017-11-03 23:38:22 -0700

    Connect settings to redux state

---
+++ src/store/reducers/settings.js
@@ -0,0 +1,30 @@
+import getInitialState from '../initial-state';
+import { actions } from '../actions/settings';
+
+const initialState = getInitialState().settings;
+
+export default function reducer(state = initialState, action) {
+
+  if (action.type === actions.OPEN_MOADL) {
+    return Object.assign({}, state, { showModal: true });
+  }
+  
+  if (action.type === actions.CLOSE_MODAL) {
+    return Object.assign({}, state, { showModal: false });
+  }
+
+  if (action.type === actions.CHANGE_SIZE) {
+    return Object.assign({}, state, { size: action.payload });
+  }
+
+  if (action.type === actions.CHANGE_LENGTH) {
+    return Object.assign({}, state, { length: action.payload });
+  }
+
+  if (action.type === actions.TOGGLE_TIMER) {
+    const timer = !state.timer;
+    return Object.assign({}, state, { timer: timer });
+  }
+
+  return state;
+};
