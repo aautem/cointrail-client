42aff9064714f5d4967dc21be07de118eb5899f1
Alex Autem <autem.alex@gmail.com>
2017-11-09 01:42:12 -0800

    Build android app

--- index.android.js
+++ index.android.js
@@ -1,3 +1,3 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
-AppRegistry.registerComponent('Contrail', () => App);
+AppRegistry.registerComponent('ContrailConnect4', () => App);
