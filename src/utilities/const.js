09561d51d2dacfeb93df1c40a6942f1740fbe21c
Alex Autem <autem.alex@gmail.com>
2017-11-08 23:09:23 -0800

    Update constants

--- src/utilities/const.js
+++ src/utilities/const.js
@@ -1,5 +1,12 @@
 export const APP_TITLE = '.~::  C O N T R A I L  ::~.';
 export const API_URL = 'https://contrail-api.herokuapp.com';
+export const POINTS = [5, 10, 25, 50, 100, 150];
+
+export const POINT_COUNTS = {
+  4: [4, 4, 4, 2, 1, 1], // 16 spaces
+  5: [6, 6, 6, 4, 2, 1], // 25 spaces
+  6: [10, 8, 8, 6, 2, 2], // 36 spaces
+};
 
 export const APP_PAGES = {
   AUTH: 'auth',
@@ -11,11 +18,4 @@ export const AUTH_PAGES = {
   LOGIN: 'login',
   SIGNUP: 'signup',
   FORGOT_PASSWORD: 'forgot-password',
-};
-
-export const POINTS = [5, 10, 25, 50, 100, 150];
-export const POINT_COUNTS = {
-  4: [4, 4, 4, 2, 1, 1], // 16 spaces
-  5: [6, 6, 6, 4, 2, 1], // 25 spaces
-  6: [10, 8, 8, 6, 2, 2], // 36 spaces
 };
